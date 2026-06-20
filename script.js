f3d4dee284352093fe00eac08b850ca49d0f1c40
RD0006 <radhikadn2022@gmail.com>
2026-06-20 10:10:10 +0530

    Buttons

--- script.js
+++ script.js
@@ -13,19 +13,50 @@ btn.addEventListener("click", () => {
     const toDoItem = document.createElement("div");
     toDoItem.classList.add("li");
 
-    toDoItem.innerHTML = `
-                <div class="toDoItem-1">
-                    <span class="category-span ${category}">${category}</span>
-                    <h3>${value}</h3>
-                </div>
-                <div>
-                    <button class="btn edit">Edit</button>
-                    <button class="btn del">Delete</button>
-                </div>
-            </div>
+    toDoItem.innerHTML = 
+    `
+        <div class="toDoItem-1">
+            <input class="input-checkbox" type="checkbox"/>
+            <span class="category-span ${category}">${category}</span>
+            <h3>${value}</h3>
+        </div>
+        <div>
+            <button class="btn edit">Edit</button>
+            <button class="btn del">Delete</button>
+        </div>
     `
 
     toDoBox.appendChild(toDoItem);
 
     inp.value = "";
+});
+
+toDoBox.addEventListener("click", (e) => {
+    if (e.target.classList.contains("del")) {
+        e.target.closest(".li").remove();
+    }
+
+    if (e.target.classList.contains("edit")) {
+        const task = e.target.closest(".li");
+        const title = task.querySelector("h3");
+
+        const currTask = title.textContent;
+
+        title.innerHTML = 
+        `
+            <input type="text" value="${currTask}" class="edit-input">
+        `;
+
+        const input = title.querySelector("input");
+        input.focus();
+
+        input.addEventListener("blur", () => {
+            title.textContent = input.value.trim() || currTask;
+        });
+
+        input.addEventListener("keydown", (e) => {
+            if (e.key == "Enter") 
+                title.textContent = input.value.trim() || currTask;
+        });
+    }
 });
