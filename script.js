19c2915eb6bbe36fa29b1c1633aa4f8349623348
RD0006 <radhikadn2022@gmail.com>
2026-06-20 07:47:16 +0530

    Add Task

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-console.log("Hello, World!");
+const inp = document.querySelector("input");
+const categoryInp = document.querySelector("select");
+const btn = document.querySelector("#add");
+const toDoBox = document.querySelector(".to-do-list");
+
+btn.addEventListener("click", () => {
+    const value = inp.value;
+    const category = categoryInp.value;
+
+    if (value.trim() === "") 
+        return;
+
+    const toDoItem = document.createElement("div");
+    toDoItem.classList.add("li");
+
+    toDoItem.innerHTML = `
+                <div class="toDoItem-1">
+                    <span class="category-span">${category}</span>
+                    <h3>${value}</h3>
+                </div>
+                <div>
+                    <button class="btn edit">Edit</button>
+                    <button class="btn del">Delete</button>
+                </div>
+            </div>
+    `
+
+    toDoBox.appendChild(toDoItem);
+
+    inp.value = "";
+});
